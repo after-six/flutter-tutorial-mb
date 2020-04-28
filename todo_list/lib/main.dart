1eabcda624988584beff07c64ce28b0a3a78b5bf
mint-blue <mint.with.blue@gmail.com>
2020-04-28 21:30:55 +0900

    add reverse button and enter input

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -4,43 +4,60 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyListViewState createState() => _MyListViewState();
+}
+
+class _MyListViewState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    List<String> aaaa = [
-      "blablalbla",
-      "blablalbla",
-      "blablalbla",
-      "blablalbla",
-      "blablalbla",
-      "blablalbla",
-      "blablalbla",
-      "blablalbla",
-      "blablalbla"
-    ];
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          primarySwatch: Colors.red, secondaryHeaderColor: Colors.orange),
-      home: Scaffold(
-        appBar: AppBar(title: Text("After todo")),
-        body: Column(
-          children: <Widget>[
-            TodoTextField(),
-            Expanded(child: _myListView(context))
-          ],
-        ),
-      ),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primarySwatch: Colors.red, secondaryHeaderColor: Colors.orange),
+        home: Scaffold(
+            appBar: AppBar(title: Text("After todo")),
+            body: Column(
+              children: <Widget>[
+                TodoTextField(),
+                Expanded(child: _myListView(context))
+              ],
+            ),
+            bottomNavigationBar: BottomAppBar(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: () => _reverse(),
+                    child: Text('Reverse items'),
+                    textColor: Colors.lightBlue,
+                  ),
+                ],
+              ),
+            )));
   }
+
+  void _reverse() {
+    setState(() {
+      europeanCountries = europeanCountries.reversed.toList();
+    });
+  }
+
 }
 
-class TodoTextField extends StatelessWidget {
-  const TodoTextField({
-    Key key,
-  }) : super(key: key);
+class TodoTextField extends StatefulWidget {
+  const TodoTextField({Key key, this.data}) : super(key: key);
+  final String data;
 
+  @override
+  _TodoTextField createState() => _TodoTextField();
+}
+
+class _TodoTextField extends State<TodoTextField> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -51,15 +68,14 @@ class TodoTextField extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Flexible(
-            flex: 9,
-            child: TextField(
-              maxLength: 25,
-              decoration: InputDecoration(
-                  counterStyle: TextStyle(fontSize: 0),
-                  hintText: "What do you gonna do?",
-                  border: InputBorder.none),
-            ),
-          ),
+              flex: 9,
+              child: TextFormField(
+                  maxLength: 25,
+                  decoration: InputDecoration(
+                      counterStyle: TextStyle(fontSize: 0),
+                      hintText: "What do you gonna do?",
+                      border: InputBorder.none),
+                  onFieldSubmitted: _handleSubmission)),
           Flexible(
             flex: 1,
             child: IconButton(
@@ -73,70 +89,80 @@ class TodoTextField extends StatelessWidget {
       ),
     );
   }
+
+  void _handleSubmission(String text) {
+    setState(() {
+      europeanCountries.insert(0, text);
+    });
+  }
+
 }
 
-Widget _myListView(BuildContext context) {
-  // backing data
-  final europeanCountries = [
-    'Albania',
-    'Andorra',
-    'Armenia',
-    'Austria',
-    'Azerbaijan',
-    'Belarus',
-    'Belgium',
-    'Bosnia and Herzegovina',
-    'Bulgaria',
-    'Croatia',
-    'Cyprus',
-    'Czech Republic',
-    'Denmark',
-    'Estonia',
-    'Finland',
-    'France',
-    'Georgia',
-    'Germany',
-    'Greece',
-    'Hungary',
-    'Iceland',
-    'Ireland',
-    'Italy',
-    'Kazakhstan',
-    'Kosovo',
-    'Latvia',
-    'Liechtenstein',
-    'Lithuania',
-    'Luxembourg',
-    'Macedonia',
-    'Malta',
-    'Moldova',
-    'Monaco',
-    'Montenegro',
-    'Netherlands',
-    'Norway',
-    'Poland',
-    'Portugal',
-    'Romania',
-    'Russia',
-    'San Marino',
-    'Serbia',
-    'Slovakia',
-    'Slovenia',
-    'Spain',
-    'Sweden',
-    'Switzerland',
-    'Turkey',
-    'Ukraine',
-    'United Kingdom',
-    'Vatican City'
-  ];
 
+
+// backing data
+List<String> europeanCountries = <String>[
+  'Albania',
+  'Andorra',
+  'Armenia',
+  'Austria',
+  'Azerbaijan',
+  'Belarus',
+  'Belgium',
+  'Bosnia and Herzegovina',
+  'Bulgaria',
+  'Croatia',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Estonia',
+  'Finland',
+  'France',
+  'Georgia',
+  'Germany',
+  'Greece',
+  'Hungary',
+  'Iceland',
+  'Ireland',
+  'Italy',
+  'Kazakhstan',
+  'Kosovo',
+  'Latvia',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Macedonia',
+  'Malta',
+  'Moldova',
+  'Monaco',
+  'Montenegro',
+  'Netherlands',
+  'Norway',
+  'Poland',
+  'Portugal',
+  'Romania',
+  'Russia',
+  'San Marino',
+  'Serbia',
+  'Slovakia',
+  'Slovenia',
+  'Spain',
+  'Sweden',
+  'Switzerland',
+  'Turkey',
+  'Ukraine',
+  'United Kingdom',
+  'Vatican City'
+];
+
+Widget _myListView(BuildContext context) {
   return ListView.separated(
     itemCount: europeanCountries.length,
     separatorBuilder: (context, index) => Divider(),
     itemBuilder: (context, index) {
       return ListTile(
         title: Text(europeanCountries[index]),
+        trailing: Icon(Icons.keyboard_arrow_right),
       );
     },
   );
